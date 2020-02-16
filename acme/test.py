13e059001e6099270da59f6b53485b442e486835
robert <robert.ayrapetyan@gmail.com>
2020-02-16 14:57:44 -0800

    Fenimore Fillmore: 3 Skulls of the Toltecs info updates delete test files

--- acme/test.py
+++
@@ -1,9 +0,0 @@
-import acme.library.utils.pyrandr as randr
-
-# get connected screens
-cs = randr.connected_screens()[0]
-reslist = cs.available_resolutions()
-cs.set_resolution(tuple([int(x) for x in "640x480".split("x")]))
-cs.apply_settings()
-cs.set_resolution((1920, 1200))
-cs.apply_settings()
