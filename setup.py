7dfaacef10eb474bc107d4b44fdbfc6baa8bf817
robert <robert.ayrapetyan@gmail.com>
2020-05-16 12:44:25 -0700

    deps fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'ansible @ https://github.com/rayrapetyan/ansible/archive/copy_local_perf_opt.zip',
+        'ansible-base @ git+https://github.com/rayrapetyan/ansible@copy_local_perf_opt',
         'click',
     ],
     entry_points={
